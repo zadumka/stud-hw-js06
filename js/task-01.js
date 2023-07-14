12731e8a399f938916b339ecbb442f4833904d03
zadumka <90329284+zadumka@users.noreply.github.com>
2023-07-14 14:04:22 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 "use strict"
-const categoriesRef = document.getElementById("categories");
-const titleCategories = categoriesRef.children;
-const lengthAmountCategories = titleCategories.length;
-console.log("Number of categories: ", lengthAmountCategories);
+const categoriesRef = document.querySelector("#categories");
+//const titleCategories = categoriesRef.children;
+//const lengthAmountCategories = titleCategories.length;
+console.log("Number of categories: ", categoriesList.children.length);
 
 for (let category of titleCategories) {
   console.log("Category: ", category.firstElementChild.textContent);
